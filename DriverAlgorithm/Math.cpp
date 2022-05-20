6c9732b84609c285e043f08d0a13128e4f40ae92
Vojtěch Gistr <vojtech.gistr@email.cz>
2022-05-20 23:26:11 +0200

    Update Math.cpp

--- DriverAlgorithm/Math.cpp
+++ DriverAlgorithm/Math.cpp
@@ -14,8 +14,6 @@ std::pair<int, int> intersectionCoords(std::pair<int, int> A, std::pair<int, int
         || (C.first != D.first && C.second != D.second)
         || (!(ccw(A, C, D) != ccw(B, C, D) && ccw(A, B, C) != ccw(A, B, D))))
         return std::make_pair(INT_MAX, INT_MAX);
-        
-    
 
     // Line AB represented as a1*x + b1*y = c1
     int a1 = B.second - A.second;
@@ -32,7 +30,7 @@ std::pair<int, int> intersectionCoords(std::pair<int, int> A, std::pair<int, int
     if (determinant == 0)
     {
         // if lines are on themselves
-        if (A.first == C.first) {               // vertical    |
+        if (A.first == C.first) {               // vertical    
 
             if (C.second < A.second && B.second < D.second) // if line is inherit
                 return std::make_pair(A.first, A.second);
@@ -42,8 +40,6 @@ std::pair<int, int> intersectionCoords(std::pair<int, int> A, std::pair<int, int
 
             else if (D.second <= B.second)      // bottom line
                 return std::make_pair(D.first, D.second);
-
-            return std::make_pair(INT_MAX, INT_MAX);
 
         }
         else if (A.second == C.second) {        // horizontal    --
@@ -57,18 +53,17 @@ std::pair<int, int> intersectionCoords(std::pair<int, int> A, std::pair<int, int
             else if (D.first <= B.first)        // bottom line
                 return std::make_pair(D.first, D.second);
 
-            return std::make_pair(INT_MAX, INT_MAX);
         }
-        else return std::make_pair(INT_MAX, INT_MAX);
+        
+        return std::make_pair(INT_MAX, INT_MAX);
     }
     else {
         int x = (b2 * c1 - b1 * c2) / determinant;
         int y = (a1 * c2 - a2 * c1) / determinant;
 
-        if ((A.first == B.first && (A.second <= y && y <= B.second) || (A.first <= x && x <= B.first))
-            || (C.first == D.first && (C.second <= y && y <= D.second) || (C.first <= x && x <= D.first)))
+        if(((A.first <= x && x <= B.first) || (B.first <= x && x <= A.first))
+            && ((A.second <= y && y <= B.second) || (B.second <= y && y <= A.second)))
             return std::make_pair(x, y);
-
 
         return std::make_pair(INT_MAX, INT_MAX);
     }
